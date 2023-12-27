bba09cfd8601bab2b71b8838dab4fe87e9e55597
Davide Suzzarellu <147137494+DavideSuzzarellu@users.noreply.github.com>
2023-12-27 11:38:58 +0000

    feat: Card

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+
+export const Card = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col d-flex justify-content-center mt-2">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body border bt-1">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div className="card-footer">
+                            <a href="#" className="btn btn-primary">Find out more!</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col d-flex justify-content-center mt-2">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body border bt-1">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div className="card-footer">
+                            <a href="#" className="btn btn-primary">Find out more!</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col d-flex justify-content-center mt-2">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body border bt-1">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div className="card-footer">
+                            <a href="#" className="btn btn-primary">Find out more!</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col d-flex justify-content-center mt-2">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body border bt-1">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div className="card-footer">
+                            <a href="#" className="btn btn-primary">Find out more!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
