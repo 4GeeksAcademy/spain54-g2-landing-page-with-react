dd155bf9a519cb089312fa706c8f548c968575c2
Davide Suzzarellu <147137494+DavideSuzzarellu@users.noreply.github.com>
2023-12-28 17:22:20 +0000

    feat: Home

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,59 +1,28 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
-export const Card = () => {
+export const Card = ({ valores = [{
+    titulo: "Card title",
+    imagen: "rigo-baby.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    button: "Find out more!"
+}] }) => {
+
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col d-flex justify-content-center mt-2">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body border bt-1">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Find out more!</a>
-                        </div>
+        <div className="col d-flex flex-row justify-content-center mt-2">
+            {valores.map((item) => (
+                <div className="card" style={{ width: '18rem' }}>
+                    <img src={item.imagen} className="card-img-top" alt="..." />
+                    <div className="card-body border bt-1">
+                        <h5 className="card-title">{item.titulo}</h5>
+                        <p className="card-text">{item.description}</p>
+                    </div>
+                    <div className="card-footer text-center">
+                        <a href="#" className="btn btn-primary">{item.button}</a>
                     </div>
                 </div>
-                <div className="col d-flex justify-content-center mt-2">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body border bt-1">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Find out more!</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col d-flex justify-content-center mt-2">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body border bt-1">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Find out more!</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col d-flex justify-content-center mt-2">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="rigo-baby.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body border bt-1">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Find out more!</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
+
--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 
 export const Jumbotron = () => {
-    return {
-        < div className="p-5 text-center bg-body-tertiary rounded-3" >
-    <svg className="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100"><use xlink:href="#bootstrap"></use></svg>
-    <h1 className="text-body-emphasis">Jumbotron with icon</h1>
-    <p className="col-lg-8 mx-auto fs-5 text-muted">
-      This is a custom jumbotron featuring an SVG image at the top, some longer text that wraps early thanks to a responsive <code>.col-*</code> className, and a customized call to action.
-    </p>
-    <div className="d-inline-flex gap-2 mb-5">
-      <button className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" type="button">
-        Call to action
-        <svg className="bi ms-2" width="24" height="24"><use xlink:href="#arrow-right-short"></use></svg>
-      </button>
-      <button className="btn btn-outline-secondary btn-lg px-4 rounded-pill" type="button">
-        Secondary link
-      </button>
-    </div>
-  </div >
-    }
-    
+  return (
+      <div className="container text-align-left py-5 mt-1">
+        <div className="p-5 mb-4 rounded-3" style={{ backgroundColor: '#f0f0f0' }}>
+          <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+          <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+          <button className="btn btn-primary btn-lg" type="button">Example button</button>
+        </div>
+      </div>
+  )
 }
